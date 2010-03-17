bf3dff3de135a82131e4177ffbed6841ef3771c6
Kamil Klimkiewicz <kamil@adrem.pl>
2010-03-17 19:37:31 +0800

    Initial gausers models structure

---
+++ gausers/tests.py
@@ -0,0 +1,22 @@
+import unittest
+from appengine_django.models import BaseModel
+from google.appengine.ext import db
+from gausers.models import Role
+
+
+class RoleCreationTestCase(unittest.TestCase):
+    def setUp(self):
+        db.delete(Role.all().fetch(100))
+
+    def testCreateRole(self):
+        Role.create('test_role', 'Some description')
+        roles = Role.all().fetch(2)
+        self.assertEqual(len(roles), 1)
+        role = roles[0]
+        self.assertEqual(role.name, 'test_role')
+
+    def testGetByName(self):
+        role_a = Role.create('test_role', 'Some description')
+        role_b = Role.get_by_name('test_role')
+        self.assertEqual(role_a, role_b)
+
