495cef75b853f806c9a5c6b901add7fc5d02f6f8
pazabo <pazabo@gmail.com>
2010-03-19 11:14:05 +0100

    helper classes for creating HTML table added

---
+++ crgappspanel/tables.py
@@ -0,0 +1,62 @@
+class Table(object):
+    def __init__(self, fields, widths, select=False, sortby='', asc=True, html_width=None, css_class=None):
+        self.fields = fields          # fields presented in the table
+        self.widths = widths          # table column widths
+        self.select = select          # selection column
+        self.sortby = sortby          # field to sort by
+        self.asc = asc                # ascending?
+        self.html_width = html_width  # html width attribute
+        self.css_class = css_class    # table css classes
+    
+    def gen_js(self, objs):
+        
+    
+    def gen_html(self, objs):
+        # starting with table start tag with appropriate class
+        s = '<table'
+        if self.html_width:
+            s += ' width="%s"' % self.html_width
+        if self.css_class:
+            s += ' class="%s"' % self.css_class
+        s += '>'
+        
+        # adding column width specifiers
+        for width in self.widths:
+            s += '<col width="%s"/>' % str(width)
+        
+        # generating table head
+        s += '<thead><tr>'
+        if self.select:
+            s += '<th><input type="checkbox"/></th>'
+        for field in self.fields:
+            s += '<th><a'
+            if field.name == self.sortby:
+                if self.asc:
+                    s += ' class="asc" href="?sortby=%s&asc=false">' % field.name
+                else:
+                    s += ' class="desc" href="?sortby=%s">' % field.name
+            else:
+                s += ' href="?sortby=%s">' % field.name
+            s += '%s</a></th>' % field.caption
+        s += '</tr></thead>'
+        
+        # generating table body
+        s += '<tbody>'
+        for obj in objs:
+            s += '<tr>'
+            if self.select:
+                s += '<td><input type="checkbox"/></td>'
+            for field in self.fields:
+                s += '<td>%s</td>' % str(obj[field.name])
+            s += '</tr>'
+        s += '</tbody>'
+        
+        # closing table tag
+        s += '</table>'
+        
+        return s
+
+class Field(object):
+    def __init__(self, name, caption):
+        self.name = name
+        self.caption = caption
--- crgappspanel/views.py
+++ crgappspanel/views.py
@@ -1,7 +1,56 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from crgappspanel.tables import Table, Field
+
+objects = [
+    {
+        'name': 'Pawel Zaborski',
+        'username': 'pawel@moroccanholidayrental.com',
+        'status': 'Administrator',
+        'quota': '0% of 25 GB',
+        'roles': 'admin',
+        'last_login': '9:28 pm',
+    },
+    {
+        'name': 'Roland Plaszowski',
+        'username': 'roland@moroccanholidayrental.com',
+        'status': 'Administrator',
+        'quota': '0% of 25 GB',
+        'roles': 'admin',
+        'last_login': 'Jan 6',
+    },
+    {
+        'name': 'Kamil Klimkiewicz',
+        'username': 'kamil@moroccanholidayrental.com',
+        'status': '',
+        'quota': '0% of 25 GB',
+        'roles': 'admin',
+        'last_login': '3:15 pm',
+    },
+    {
+        'name': 'sky mail',
+        'username': 'skymail@moroccanholidayrental.com',
+        'status': '',
+        'quota': '3% of 25 GB',
+        'roles': 'sample',
+        'last_login': 'Mar 15',
+    },
+]
+
+fields = [
+    Field('name', 'Name'),
+    Field('username', 'Username'),
+    Field('status', 'Status'),
+    Field('quota', 'Email quota'),
+    Field('roles', 'Roles'),
+    Field('last_login', 'Last signed in')
+]
+widths = [
+    '5%', '15%', '25%', '15%', '15%', '15%', '10%'
+]
+fieldNames = [field.name for field in fields]
 
 def users(request):
-    if 'sortby' in request.GET:
+    if ('sortby' in request.GET) and (request.GET['sortby'] in fieldNames):
         sortby = request.GET['sortby']
     else:
         sortby = 'name'
@@ -23,41 +72,8 @@ def users(request):
         else:
             return 0
     
-    objects = [
-        {
-            'name': 'Pawel Zaborski',
-            'username': 'pawel@moroccanholidayrental.com',
-            'status': 'Administrator',
-            'quota': '0% of 25 GB',
-            'roles': 'admin',
-            'last_login': '9:28 pm',
-        },
-        {
-            'name': 'Roland Plaszowski',
-            'username': 'roland@moroccanholidayrental.com',
-            'status': 'Administrator',
-            'quota': '0% of 25 GB',
-            'roles': 'admin',
-            'last_login': 'Jan 6',
-        },
-        {
-            'name': 'Kamil Klimkiewicz',
-            'username': 'kamil@moroccanholidayrental.com',
-            'status': '',
-            'quota': '0% of 25 GB',
-            'roles': 'admin',
-            'last_login': '3:15 pm',
-        },
-        {
-            'name': 'sky mail',
-            'username': 'skymail@moroccanholidayrental.com',
-            'status': '',
-            'quota': '3% of 25 GB',
-            'roles': 'sample',
-            'last_login': 'Mar 15',
-        },
-    ]
-    
     objects.sort(compare)
     
-    return render_to_response('users_list.html', {'objects': objects, 'sortby': sortby, 'asc': asc})
+    table = Table(fields, widths, select=True, sortby=sortby, asc=asc, html_width='100%', css_class='data')
+    
+    return render_to_response('users_list.html', {'table_js': table.gen_js(objects), 'table_html': table.gen_html(objects)})
