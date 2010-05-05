21dc60e4ef9af81ac5815078ad6ca38b97451fa1
pazabo <pazabo@gmail.com>
2010-05-06 00:12:45 +0200

    Bugfix

--- crlib/mappers.py
+++ crlib/mappers.py
@@ -402,7 +402,7 @@ class _EmailSettingsWrapper(object):
     MAIL_FROM_NOW_ON = 'MAIL_FROM_NOW_ON'
 
     def __init__(self, user_name):
-        from crlib import users
+        from auth import users
         from gdata.apps.emailsettings.service import EmailSettingsService
 
         self._user_name = user_name
