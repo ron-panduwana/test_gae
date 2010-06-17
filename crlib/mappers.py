7e8c44bdc1193ac511524497fc82347c03b35a2a
Kamil Klimkiewicz <miGlanz@gmail.com>
2010-06-17 18:13:54 +0200

    Exception declarations moved to new module: errors

--- crlib/mappers.py
+++ crlib/mappers.py
@@ -9,6 +9,7 @@ from gdata.apps import PropertyEntry
 from gdata.apps.groups import service as groups
 from atom import AtomBase
 from crlib.gdata_wrapper import AtomMapper, simple_mapper, StringProperty
+from crlib.errors import apps_for_your_domain_exception_wrapper
 
 
 # Constants
@@ -71,59 +72,6 @@ GROUP_EMAIL_PERMISSIONS = (
     (groups.PERMISSION_DOMAIN, _('Domain')),
     (groups.PERMISSION_ANYONE, _('Anyone')),
 )
-
-
-# Exceptions
-
-class GDataError(Exception): pass
-
-
-class EntityDeletedRecentlyError(GDataError):
-    """ The request instructs Google to create a new entity but uses
-    the name of an entity that was deleted in the previous five days.
-
-    """
-
-
-class DomainUserLimitExceededError(GDataError):
-    """ The specified domain has already reached its quota of user accounts.
-    
-    """
-
-
-class EntityExistsError(GDataError):
-    """ The request instructs Google to create an entity that already exists.
-
-    """
-
-
-class EntityDoesNotExistError(GDataError):
-    """ The request asks Google to retrieve an entity that does not exist.
-
-    """
-
-
-APPS_FOR_YOUR_DOMAIN_ERROR_CODES = {
-    1100: EntityDeletedRecentlyError,
-    1200: DomainUserLimitExceededError,
-    1300: EntityExistsError,
-    1301: EntityDoesNotExistError,
-}
-
-
-def apps_for_your_domain_exception_wrapper(func):
-    from gdata.apps.service import AppsForYourDomainException
-    def new(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except AppsForYourDomainException, e:
-            exception = APPS_FOR_YOUR_DOMAIN_ERROR_CODES.get(e.error_code)
-            if exception:
-                raise exception()
-            raise
-    new.__name__ = func.__name__
-    new.__doc__ = func.__doc__
-    return new
 
 
 # Mappers
